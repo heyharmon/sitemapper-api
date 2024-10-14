2e181342f6487b94624a072d9b11c9d81bab8813
Ryan Harmon <ryan@bloomcu.com>
2024-10-13 23:45:31 -0600

    Bump

--- app/Http/Websites/WebsiteController.php
+++ app/Http/Websites/WebsiteController.php
@@ -6,37 +6,38 @@ use Throwable;
 use Illuminate\Http\Request;
 use DDD\Domain\Websites\Website;
 use DDD\Domain\Websites\Resources\WebsiteResource;
+use DDD\Domain\Companies\Company;
 use DDD\App\Controllers\Controller;
 
 class WebsiteController extends Controller
 {
-    public function index()
+    public function index(Company $company)
     {
-        $websites = Website::paginate(20);
+        $websites = $company->websites;
 
         return WebsiteResource::collection($websites);
     }
 
-    public function store(Request $request)
+    public function store(Company $company, Request $request)
     {
-        $website = Website::create($request->all());
+        $website = $company->websites()->create($request->all());
 
         return new WebsiteResource($website);
     }
 
-    public function show(Website $website)
+    public function show(Company $company, Website $website)
     {
         return new WebsiteResource($website);
     }
 
-    public function update(Website $website, Request $request)
+    public function update(Company $company, Website $website, Request $request)
     {
         $website->update($request->all());
 
         return new WebsiteResource($website);
     }
 
-    public function destroy(Website $website)
+    public function destroy(Company $company, Website $website)
     {
         $website->delete();
 
